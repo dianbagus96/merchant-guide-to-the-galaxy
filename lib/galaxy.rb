8a1458e600b509e431d5dccaa26babe41bfb7bd9
Felipe Roveran <feliperoveran@hotmail.com>
2018-03-06 01:21:31 -0300

    move unit value fetching to a private method so its behavior is isolated

--- lib/galaxy.rb
+++ lib/galaxy.rb
@@ -1,7 +1,7 @@
 require './lib/roman'
 
 class Galaxy
-  attr_reader :units
+  attr_reader :units, :roman
 
   def initialize(roman = Roman)
     @units = {}
@@ -13,8 +13,14 @@ class Galaxy
   end
 
   def value_of(galactic_units)
-    roman_values = galactic_units.map { |unit_name| units[unit_name] }
+    roman_values = galactic_units.map { |unit_name| value_of_unit(unit_name) }
 
-    @roman.to_i(roman_values.join)
+    roman.to_i(roman_values.join)
+  end
+
+  private
+
+  def value_of_unit(name)
+    units[name]
   end
 end
