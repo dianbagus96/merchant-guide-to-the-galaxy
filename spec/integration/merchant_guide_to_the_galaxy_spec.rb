e31cec9b07da409261b9ebee45747c255b41e691
Felipe Roveran <feliperoveran@hotmail.com>
2018-06-11 23:00:17 -0300

    add spec to make sure nothing is printed to stderr and make debugging easier

--- spec/integration/merchant_guide_to_the_galaxy_spec.rb
+++ spec/integration/merchant_guide_to_the_galaxy_spec.rb
@@ -10,6 +10,14 @@ describe 'Merchant\'s Guide to the Galaxy' do
 
       expect(stdout).to eq expected_output
     end
+
+    it 'does not print anything to stderr' do
+      command = 'ruby merchant_guide_to_the_galaxy.rb'
+
+      _, stderr, _ = Open3.capture3(command)
+
+      expect(stderr).to be_empty
+    end
   end
 
   context 'opening a different file' do
@@ -17,9 +25,18 @@ describe 'Merchant\'s Guide to the Galaxy' do
       command = 'ruby merchant_guide_to_the_galaxy.rb spec/fixtures/test_input.txt'
       expected_output = File.open('spec/fixtures/test_output.txt').read
 
-      stdout, _, _ = Open3.capture3(command)
+      stdout, stderr, _ = Open3.capture3(command)
 
+      expect(stderr).to be_empty
       expect(stdout).to eq expected_output
+    end
+
+    it 'does not print anything to stderr' do
+      command = 'ruby merchant_guide_to_the_galaxy.rb'
+
+      _, stderr, _ = Open3.capture3(command)
+
+      expect(stderr).to be_empty
     end
   end
 
@@ -31,6 +48,14 @@ describe 'Merchant\'s Guide to the Galaxy' do
 
       expect(stdout)
         .to match(/Invalid file! Opening 'inexistent_file\.txt' returned: No such file/)
+    end
+
+    it 'does not print anything to stderr' do
+      command = 'ruby merchant_guide_to_the_galaxy.rb'
+
+      _, stderr, _ = Open3.capture3(command)
+
+      expect(stderr).to be_empty
     end
   end
 end
